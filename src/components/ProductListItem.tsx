da0ccafaf879321892df03acf0a4ff1c413a8b8f
ataberkakkan <akkanataberk@gmail.com>
2024-02-15 11:33:06 +0300

    Product Card Component

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,44 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Product } from "../types";
+
+import Colors from "../constants/Colors";
+
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View style={styles.container}>
+      <Image src={product.image || defaultPizzaImage} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "700",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+});
