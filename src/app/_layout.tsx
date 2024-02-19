fc51914e9f4587c4933e933f0c2f3d3f076ef5a1
ataberkakkan <akkanataberk@gmail.com>
2024-02-19 10:35:14 +0300

    Changed layouts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -11,6 +11,8 @@ import { useEffect } from "react";
 
 import { useColorScheme } from "@/src/components/useColorScheme";
 
+import CartProvider from "../providers/CartProvider";
+
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
@@ -53,10 +55,12 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-      </Stack>
+      <CartProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="cart" options={{ presentation: "modal" }} />
+        </Stack>
+      </CartProvider>
     </ThemeProvider>
   );
 }
