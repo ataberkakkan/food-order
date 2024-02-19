4b675f047537036b4667c8e3318ff0903f57cb5a
ataberkakkan <akkanataberk@gmail.com>
2024-02-19 10:34:50 +0300

    Add to cart functionality

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,20 +1,32 @@
-import { Stack, useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import { useState } from "react";
+import { useCart } from "@/src/providers/CartProvider";
 
 import products from "@/assets/data/products";
 import Button from "@/src/components/Button";
+import { PizzaSize } from "@/src/types";
 
-const sizes = ["S", "M", "L", "XL"];
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
+  const { addItem } = useCart();
 
-  const [selectedSize, setSelectedSize] = useState("M");
+  const router = useRouter();
+
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
 
   const product = products.find((p) => p.id.toString() === id);
 
-  const addToCart = () => {};
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    addItem(product, selectedSize);
+
+    router.push("/cart");
+  };
 
   if (!product) {
     return <Text>Product Not Found</Text>;
